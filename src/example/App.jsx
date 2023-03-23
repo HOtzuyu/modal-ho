a15ea892f75a68385ccb46490060f9eb7e4e5dcb
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2023-03-23 17:06:55 +0100

    test

--- src/example/App.jsx
+++ src/example/App.jsx
@@ -1,30 +1,99 @@
 import React, { useState } from "react";
-import Modal from "../lib/Modal";
-const ParentComponent = () => {
-  const [showModal, setShowModal] = useState(false);
+import { Modale } from "../lib/Modal";
+import "./App.css";
 
-  const handleClick = () => {
-    setShowModal(true);
+function App() {
+  const [displayModal, setDisplayModal] = useState(false);
+  const modalStyle = {
+    background: "#4343ab",
+    margin: "auto",
+    borderRadius: "10px",
+    maxWidth: "400px",
+    width: "80%",
+    padding: "0.5rem",
+    border: "1px solid white",
   };
 
-  const handleClose = () => {
-    setShowModal(false);
+  const modalHeaderStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0.2rem 0.5rem 1.5rem",
+  };
+
+  const titleStyle = {
+    color: "pink",
+  };
+
+  const modalBodyStyle = {
+    fontSize: "20px",
+    color: "red",
+    textAlign: "center",
+  };
+
+  const modalFooterStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: "1rem",
+    marginTop: "2rem",
+  };
+
+  const xButtonStyle = {
+    fontSize: "1rem",
+    fontWeight: "700",
+    color: "rgb(39, 39, 39)",
+    cursor: "pointer",
+    border: "none",
+    background: "transparent",
+  };
+
+  const footerButton1Style = {
+    background: "yellow",
+    color: "red",
+    width: "20%",
+    cursor: "pointer",
+    padding: "0.5rem",
+    margin: "0.5rem",
+    border: "1px solid rgb(39, 39, 39)",
+  };
+
+  const footerButton2Style = {
+    background: "lightblue",
+    color: "red",
+    width: "50%",
+    cursor: "pointer",
+    padding: "0.5rem",
+    margin: "0.5rem",
+    border: "1px solid rgb(39, 39, 39)",
   };
 
   return (
-    <div>
-      <button onClick={handleClick}>Open Modal</button>
-      {showModal ? (
-        <Modal
-          title="Modal Title"
-          message="Modal Message"
-          onClose={handleClose}
-        />
-      ) : (
-        <></>
-      )}
+    <div className="App">
+      <button
+        className="modal-toggle"
+        onClick={() => setDisplayModal(true)}
+      >
+        show modal
+      </button>
+      <Modale
+        modalStyle={modalStyle}
+        modalHeaderStyle={modalHeaderStyle}
+        titleStyle={titleStyle}
+        modalBodyStyle={modalBodyStyle}
+        modalFooterStyle={modalFooterStyle}
+        xButtonStyle={xButtonStyle}
+        footerButton1Style={footerButton1Style}
+        footerButton2Style={footerButton2Style}
+        title="Enter your modal title here"
+        message="Hello, your message here"
+        showModal={displayModal}
+        hideModal={() => setDisplayModal(false)}
+        buttonText1="Ok"
+        buttonText2="Cancel"
+      />
     </div>
   );
-};
+}
 
-export default ParentComponent;
+export default App;
--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -1,40 +1,8 @@
 import React, { useState } from "react";
 import "./Modal.css";
 
-const Modale = ({ title, message, onClose }) => {
-  const [showModal, setShowModal] = useState(true);
-
-  const handleClose = () => {
-    setShowModal(false);
-    onClose();
-  };
-
-  return (
-    <div className={`modal ${showModal ? "show" : ""}`}>
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">{title}</h5>
-          <button
-            type="button"
-            className="close"
-            onClick={handleClose}
-          >
-            X
-          </button>
-        </div>
-        <div className="modal-body">{message}</div>
-        <div className="modal-footer">
-          <button
-            type="button"
-            className="btn btn-secondary"
-            onClick={handleClose}
-          >
-            Close
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+const Modale = () => {
+  return <div>Hello i m a modal</div>;
 };
 
 export default Modale;
